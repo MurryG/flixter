49609a9269f356024258702ed3474b5253eaaf31
MurryG <mercedes.gasser@uwalumni.com>
2020-01-16 02:25:16 +0000

    Add video uploading functionality.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,13 @@
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
-  config.storage = :aws                             # required
-  config.aws_bucket = ENV.fetch('AWS_BUCKET')       # required
+  config.storage = :aws                                # required
+  config.aws_bucket = ENV['AWS_BUCKET']                # required
   config.aws_acl = 'private'
 
   config.aws_credentials = {
-    access_key_id:     ENV['AWS_ACCESS_KEY_ID'],               # required
-    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],            # required
-    region:            ENV.fetch('AWS_REGION')      # REQUIRED
+    access_key_id:     ENV['AWS_ACCESS_KEY_ID'],       # required
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],   # required
+    region:            ENV['AWS_REGION']               # required
   }
 end
